2062fd4c639ab99b4bb72049fdec5b883e39cf27
Mushawiruddin Muhtar <angkomuhtar@gmail.com>
2022-04-14 06:52:16 +0800

    pegawai

--- pages/api/_middleware.js
+++ pages/api/_middleware.js
@@ -3,13 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 
 export default function middleware(Req) {
   const Auth = Req.headers.get("authorization");
-  //   console.log(Req.headers?.authorization);
   if (Req.url.includes("api/auth/")) {
     return NextResponse.next();
   }
 
   if (!Auth) {
-    return new Response(JSON.stringify({ message: Req.url }), {
+    return new NextResponse(JSON.stringify({ message: Req.url }), {
       status: 401,
     });
   }
