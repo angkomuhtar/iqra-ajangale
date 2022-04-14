2c658bf44d13f565a9cb7186fd875d277b5c0959
Mushawiruddin Muhtar <angkomuhtar@gmail.com>
2022-04-14 09:45:47 +0800

    pegawai

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,23 @@
-// Update with your config settings.
-
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
 module.exports = {
-  development: {
+  staging: {
     client: "mysql",
     connection: {
-      host: "127.0.0.1",
+      host: "us-cdbr-east-05.cleardb.net",
       port: 3306,
-      user: "root",
-      password: "",
-      database: "iqra-next",
+      user: "ba80cda7d248ed",
+      password: "41b108c9",
+      database: "heroku_d5759c96f214167",
+    },
+  },
+
+  development: {
+    client: process.env.DB_CLIENT || "mysql",
+    connection: {
+      host: process.env.DB_HOST || "127.0.0.1",
+      port: 3306,
+      user: process.env.DB_USERNAME || "root",
+      password: process.env.DB_PASSWORD || "",
+      database: process.env.DB_NAME || "iqra-next",
     },
   },
 };
--- migrations/20220414004245_create_view_berkas_kp4TEs_table.js
+++ migrations/20220414004245_create_view_berkas_kp4TEs_table.js
@@ -2,14 +2,14 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  knex.schema.createTable("test", function (table) {
+    table.string("mauku");
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};
