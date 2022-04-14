f5e32e0d701e90327550d2d40f5da80d534f2eda
Mushawiruddin Muhtar <angkomuhtar@gmail.com>
2022-04-14 08:03:24 +0800

    pegawai

--- pages/api/ddleware.js
+++ pages/api/ddleware.js
@@ -11,29 +11,29 @@
 //   //   }
 //   //   return NextResponse.next();
 // }]
-import jwt from "jsonwebtoken";
-import { NextRequest, NextResponse } from "next/server";
+// import jwt from "jsonwebtoken";
+// import { NextRequest, NextResponse } from "next/server";
 
-export default function middleware(request, event) {
-  const Bearer = request.headers.get("Authorization");
-  let pathname = request.nextUrl.pathname;
-  if (pathname.includes("api/auth/")) return NextResponse.next();
-  if (!Bearer) {
-    return Response("Unauthorized", {
-      headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
-      status: 401,
-    });
-  } else {
-    const [authType, authToken] = Bearer.split(" ");
-    return jwt.verify(authToken, "secret", function (err, decoded) {
-      if (err) {
-        return Response("Unauthorized Token", {
-          headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
-          status: 401,
-        });
-      }
-      return NextResponse.next();
-    });
-    console.log(authType, authToken);
-  }
-}
+// export default function middleware(request, event) {
+//   const Bearer = request.headers.get("Authorization");
+//   let pathname = request.nextUrl.pathname;
+//   if (pathname.includes("api/auth/")) return NextResponse.next();
+//   if (!Bearer) {
+//     return Response("Unauthorized", {
+//       headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
+//       status: 401,
+//     });
+//   } else {
+//     const [authType, authToken] = Bearer.split(" ");
+//     return jwt.verify(authToken, "secret", function (err, decoded) {
+//       if (err) {
+//         return Response("Unauthorized Token", {
+//           headers: { "WWW-Authenticate": 'Basic realm="Secure Area"' },
+//           status: 401,
+//         });
+//       }
+//       return NextResponse.next();
+//     });
+//     console.log(authType, authToken);
+//   }
+// }
