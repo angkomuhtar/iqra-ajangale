50e4aa88dacc620a121f6c57173743ad611a1c8d
Mushawiruddin Muhtar <angkomuhtar@gmail.com>
2022-04-24 00:02:40 +0800

    push

--- components/Cards/Table/FieldList.js
+++ components/Cards/Table/FieldList.js
@@ -14,6 +14,7 @@ const FieldList = ({ data }) => {
             <a
               href={`/berkas/56/${data}`}
               target="_blank"
+              rel="noreferrer"
               className="flex justify-center items-center"
             >
               <MdCloudDownload />{" "}
